f2ff2600bd2819e25bf36eab251112e347862275
Gendo <balaganshop@mail.ru>
2023-01-07 12:44:09 +0300

    Add XOR in Application.py

--- Application.py
+++ Application.py
@@ -2,3 +2,11 @@ print("NewPrint")
 print("Hello, World")
 print("Hello, World")
 print("Hello, World")
+a = 15
+b = 32
+
+# Finding XOR of a and b using ^ operator
+c = a ^ b
+
+# Printing the XOR value
+print(a, "XOR", b, "=", c)
